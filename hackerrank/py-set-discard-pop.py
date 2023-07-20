db4ac14992765112d09274d6fc8542a619d816b7
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-07-20 09:46:27 +0530

    py-set-discard-remove-pop

---
+++ hackerrank/py-set-discard-pop.py
@@ -0,0 +1,16 @@
+#https://www.hackerrank.com/challenges/py-set-discard-remove-pop/problem
+n = int(input())
+s = list(map(int, input().split()))
+m = int(input())
+ls = []
+for i in range(m):
+    ls.append(input().split())
+# print(ls)
+
+for i in ls:
+    if i[0]=='pop':
+        s = s[1:]
+    if i[0]=='discard' or i[0]=='remove':
+        if int(i[1]) in s :
+            s.remove(int(i[1]))
+print(sum(s))

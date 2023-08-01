1429d566f19e7cb94445b51f808d8ac6016b9b95
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-08-01 23:15:08 +0530

    reduce function

--- hackerrank/vaidate-email-address-filter.py
+++ hackerrank/vaidate-email-address-filter.py
@@ -1,33 +1,11 @@
-#pending
+#https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem
+
+import re
+
 def fun(s):
-    flag = True
-    if not '@' in s or (s.count('@')>1):
-        # print('@')
-        return False 
-    if not '.' in s or (s.count('.')>1):
-        # print('.')
-        return False
-    u = s.split('@')[0]
-    w = (s.split('@')[1]).split('.')[0]
-    e = (s.split('@')[1]).split('.')[1]
-    # print(u,w,e)
-    
-    for i in u:
-        if i.isalpha() or i.isdigit() or i=='-' or i=='_':
-            # print(i)
-            flag = True
-        else:
-            # print('1')
-            flag = False 
-    for i in e:
-        if not i.isalpha():
-            # print('2')
-            flag = False 
-    if len(e)>3:
-        # print('3')
-        flag = False
-    # print(s,flag)
-    return flag
+    match = re.search(r"^([\w-]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{1,3}$)" , s)
+    if match != None:
+        return bool(match)
     
     
 def filter_mail(emails):

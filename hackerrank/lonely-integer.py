4591a8a28641cbb1b516818ddaebc29243d23e2c
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-10-08 23:45:40 +0530

    lonely-integer

---
+++ hackerrank/lonely-integer.py
@@ -0,0 +1,33 @@
+
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    for i in a :
+        if a.count(i)==1:
+            return i
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

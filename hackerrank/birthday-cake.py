f17e86d549515e83f9b4ad2a94eda6f8951e2792
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-11-08 23:48:11 +0530

    birthday cake candles

---
+++ hackerrank/birthday-cake.py
@@ -0,0 +1,27 @@
+#https://www.hackerrank.com/challenges/birthday-cake-candles
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    val = ar.count(max(ar))
+    return val
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

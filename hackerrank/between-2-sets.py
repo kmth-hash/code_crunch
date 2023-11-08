c09253ce6a57805be3d808901da33ea65770b564
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-11-08 23:49:51 +0530

    between two sets

--- hackerrank/between-2-sets.py
+++ hackerrank/between-2-sets.py
@@ -8,15 +8,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'getTotalX' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
-#
-
 def getTotalX(a, b):
     # Write your code here
     #print(a,b,min(b))

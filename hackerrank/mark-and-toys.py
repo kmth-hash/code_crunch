6331cc2972ec7d532e1b1082dc11fe469835bc19
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-11-24 22:55:02 +0530

    mark and toys

---
+++ hackerrank/mark-and-toys.py
@@ -0,0 +1,38 @@
+#https://www.hackerrank.com/challenges/mark-and-toys/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    res = 0
+    c = 0
+    for i in prices:
+        if res+i>=k:
+            break
+        res = res+i
+        c+=1
+    return c
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+

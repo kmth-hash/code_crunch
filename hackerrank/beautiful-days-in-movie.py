2b278ea90d6a19d38b142dad857b2e379136eff4
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-12-18 18:30:58 +0530

    beautiful days in the movie

---
+++ hackerrank/beautiful-days-in-movie.py
@@ -0,0 +1,48 @@
+#https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    sm = 0
+    for x in range(i ,j+1):
+        #print(x)
+        #print(int(x))
+        #print( int(str(x)[::-1])
+        
+        if abs(int(x)-int(str(x)[::-1]))%k==0:
+            sm +=1
+    return sm
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

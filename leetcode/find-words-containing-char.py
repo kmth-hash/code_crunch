d712c8339e28f922e89b6d8fd6b2a88f3030f408
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-12-19 22:04:06 +0530

    find words containing character

---
+++ leetcode/find-words-containing-char.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/find-words-containing-character/description/
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        res=[]
+        for i in range(len(words)):
+            if x in words[i]:
+                res.append(i)
+        return(res)

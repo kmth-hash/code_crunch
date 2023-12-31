e59710d270e4058383d6daeb097919d834762db0
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-01-01 01:20:25 +0530

    math pow method

---
+++ leetcode/pow-meth.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/powx-n/description/?envType=study-plan-v2&envId=top-interview-150
+
+from math import pow
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)

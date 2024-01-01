e2b80632fa2296adf714bfce79b295dcbf538417
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-01-01 12:57:18 +0530

    Create Dataframe from list

---
+++ leetcode/create-dataframe-from-list.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/create-a-dataframe-from-list/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    rd = pd.DataFrame(student_data , columns=['student_id','age'])
+    print(rd)
+    return rd
+

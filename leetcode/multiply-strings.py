be7e71a32f21959627b6c5ef23b58a547416fac0
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-01-04 16:58:45 +0530

    Multiply strings

---
+++ leetcode/multiply-strings.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/multiply-strings/?envType=study-plan-v2&envId=programming-skills
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1)*int(num2))
+

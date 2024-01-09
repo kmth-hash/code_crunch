26f6eb00ae56dcbd7f8f5c3230de7d296fff1de1
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-01-09 20:36:16 +0530

    Fibonacci number

---
+++ leetcode/fibonacci.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0 or n==1 :
+            return n
+        d = [0]*(n+1)
+        d[0] = 0
+        d[1] = 1
+        for i in range(2,n+1):
+            d[i] = d[i-1]+d[i-2]
+        return d[n]
+            
+
+            

40b52eaf1c1605383279387ba9bd0481e259eb55
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-01-09 20:43:14 +0530

    Majority Element

---
+++ leetcode/majority-element.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/majority-element/description/
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        return nums[n//2]

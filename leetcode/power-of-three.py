26190143375baa40fac6e5d7fffead422c16cd7d
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-02-25 23:44:19 +0530

    Power of Three

--- leetcode/power-of-three.py
+++ leetcode/power-of-three.py
@@ -1,13 +1,13 @@
-# https://leetcode.com/problems/power-of-two/description/
+# https://leetcode.com/problems/power-of-three/description/
 
 class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
+    def isPowerOfThree(self, n: int) -> bool:
         x = 1
         n2 = 0
         while n>= x :
             if x==n:
                 return True
             n2+=1
-            x = pow(2,n2)
+            x = pow(3,n2)
             
         return False

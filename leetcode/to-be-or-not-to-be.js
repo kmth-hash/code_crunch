1e1f04d916d439020581cc1f9fb49d91d33f2d9c
Pranav Kamath <82401957+kmth-hash@users.noreply.github.com>
2024-03-02 23:42:52 +0530

    To be or Not to be

---
+++ leetcode/to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/to-be-or-not-to-be/?envType=study-plan-v2&envId=30-days-of-javascript
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */

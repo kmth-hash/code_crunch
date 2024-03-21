71467b181ab80638403994045fdc414eb4bb7599
Pranav Kamath <82401957+kmth-hash@users.noreply.github.com>
2024-03-21 16:22:17 +0530

    Weather Observation 6

---
+++ hackerrank/weather-observation-6.sql
@@ -0,0 +1,3 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+
+select CITY from Station where city LIKE 'a%' or city LIKE 'e%' or city LIKE 'i%' or city LIKE 'o%' or city LIKE 'u%' ;

b56fe7cfce2d10b2e027ecb5eabc6812f0951aba
Pranav Kamath <82401957+kmth-hash@users.noreply.github.com>
2024-06-21 17:13:46 +0530

    Actors and Directors who cooperated atleast 3 times

---
+++ leetcode/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,3 @@
+-- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/description/
+
+select actor_id , director_id from ActorDirector group by actor_id,director_id having count(*)>=3;

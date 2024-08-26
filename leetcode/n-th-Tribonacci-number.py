cf637803ee567469c777d3727e67d91503f77b20
Pranav Kamath <82401957+kmth-hash@users.noreply.github.com>
2024-08-26 09:53:42 +0530

    n-th Tribonacci Number

---
+++ leetcode/n-th-Tribonacci-number.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+#         a , b , c = 0 , 1, 1
+#         if n<3 : 
+#             return [0,1,1][n]
+#         while n-2!=0 :
+#             temp = a+b+c
+#             a , b, c = b ,c , temp 
+#             n -= 1 
+#             print(a,b,c,n,n-3)
+#         return(c)
+
+class Solution:
+    @cache
+    def tribonacci(self, n: int) -> int:
+        if n<=2 : 
+            return [0,1,1][n]
+        return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
+        

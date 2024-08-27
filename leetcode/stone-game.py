612524133979dee6eb414c5c920a59b704471f3b
Pranav Kamath <82401957+kmth-hash@users.noreply.github.com>
2024-08-27 23:57:36 +0530

    Stone Game

---
+++ leetcode/stone-game.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/stone-game/description/
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        # lol 
+        # Weirdly this works. I'm not kidding. 
+        return True

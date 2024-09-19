c5c9b7ee4a5a4b18ad61808c513ef812d77bfa44
Pranav Kamath <82401957+kmth-hash@users.noreply.github.com>
2024-09-20 02:18:21 +0530

    Update max-average-subarray

--- leetcode/max-average-subarray.py
+++ leetcode/max-average-subarray.py
@@ -1,14 +1,12 @@
-# Incomplete 
 # https://leetcode.com/problems/maximum-average-subarray-i/
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        mx = sum(nums[:k])/k
-
-        for i in range(len(nums)-k+1):
-            # print(i,i+k,nums[i:i+k],sum(nums[i:i+k]),k,mx)
-            # print(mx, (sum(nums[i:i+k])/k))
-            l = (sum(nums[i:i+k])/k)
-            mx = max(l,mx)
-        # print(mx)
-        return mx
+        res = sum(nums[:k])
+        curr = res
+        for i in range(k,len(nums)) : 
+            curr = curr +(nums[i]-nums[i-k])
+            res = max(res , curr)
+            
+        # print(res/k)
+        return res/k
